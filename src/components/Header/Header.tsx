44fe5bc55758f3f7ee518a9b97c31da2fea0b680
Nikita <nikita456nikita@yandex.ru>
2024-09-18 23:41:58 +0300

    refactor: change menu item wrappers to Link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { Menu } from 'components/Menu/Menu';
 import classes from './header.module.css';
+import { Link } from 'react-router-dom';
 
 export const Header = () => {
   return (
     <header className={classes.headerWrapper}>
       <div className={classes.headerContent}>
-        <img src="src/assets/museum-logo-white.svg" alt="logo" />
+        <Link to="/">
+          <img src="src/assets/museum-logo-white.svg" alt="logo" />
+        </Link>
         <Menu />
       </div>
     </header>
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,18 +1,19 @@
 import { AiOutlineHome } from 'react-icons/ai';
 import classes from './menu.module.css';
 import { PiBookmarkSimpleBold } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
 
 export const Menu = () => {
   return (
     <section className={classes.menuSection}>
-      <p className={classes.menuItem}>
+      <Link to="/" className={classes.menuItem}>
         <AiOutlineHome className={classes.iconColor} />
         Home
-      </p>
-      <p className={classes.menuItem}>
+      </Link>
+      <Link to="/favourites" className={classes.menuItem}>
         <PiBookmarkSimpleBold className={classes.iconColor} />
         Your favorites
-      </p>
+      </Link>
     </section>
   );
 };
