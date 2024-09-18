28f4ab7144670f01c7e2067d8a08d63a93754264
Nikita <nikita456nikita@yandex.ru>
2024-09-18 23:55:17 +0300

    feat: add style to mainLayout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom';
 
 export const MainLayout = () => {
   return (
-    <>
+    <div>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </div>
   );
 };
