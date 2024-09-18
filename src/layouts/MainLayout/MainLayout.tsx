5c26e7c3a1cd0798b08d38befd08ccd0a85790c4
Nikita <114023790+Nikitos32@users.noreply.github.com>
2024-09-19 00:00:22 +0300

    Feat/footer (#3)
    * feat: add footer
    
    * feat: add style to mainLayout
    
    * feat: add style to footer

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,11 +1,13 @@
+import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
 
 export const MainLayout = () => {
   return (
-    <>
+    <div>
       <Header />
       <Outlet />
-    </>
+      <Footer />
+    </div>
   );
 };
