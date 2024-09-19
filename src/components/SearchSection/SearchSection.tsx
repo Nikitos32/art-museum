5ad98f15495e57b6ef071c1d110502f3c611d52d
Nikita <nikita456nikita@yandex.ru>
2024-09-19 23:58:49 +0300

    feat: add searchSection component

---
+++ src/components/SearchSection/SearchSection.tsx
@@ -0,0 +1,11 @@
+export const SearchSection = () => {
+  return (
+    <section>
+      <p>
+        Let{"'"}s Find Some
+        <span>Art</span>
+        Here!
+      </p>
+    </section>
+  );
+};
