62c835a6e3e56baa12a3d3c084fe150543e2c6c9
Nikita <nikita456nikita@yandex.ru>
2024-09-20 01:46:53 +0300

    feat: add item component and item list on the main page

--- src/components/CatalogItem/CatalogItem.tsx
+++ src/components/CatalogItem/CatalogItem.tsx
@@ -1,5 +1,26 @@
+import { PiBookmarkSimpleBold } from 'react-icons/pi';
 import classes from './catalogItem.module.css';
 
 export const CatalogItem = () => {
-  return <p className={classes.catalogItemWrapper}>Item</p>;
+  return (
+    <div className={classes.catalogItemWrapper}>
+      <img
+        src="src/assets/test-image.png"
+        alt="image"
+        className={classes.imageSize}
+      />
+      <section className={classes.infoWrapper}>
+        <div className={classes.infoContent}>
+          <article>
+            <p className={classes.basicText}>Charles V, bust length...</p>
+            <p className={classes.orangeText}>Giovanni Britto</p>
+          </article>
+          <p className={classes.boldtext}>public</p>
+        </div>
+        <button className={classes.favouritesBtn}>
+          <PiBookmarkSimpleBold size={24} color="rgba(241, 121, 0, 1)" />
+        </button>
+      </section>
+    </div>
+  );
 };
