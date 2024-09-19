e003b4e9ba70f487232591fcad31e223be41085d
Nikita <nikita456nikita@yandex.ru>
2024-09-20 00:40:10 +0300

    feat: add fonts and gap for main section

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,11 @@
 import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
+import classes from './mainLayout.module.css';
 
 export const MainLayout = () => {
   return (
-    <div>
+    <div className={classes.mainLayoutWrapper}>
       <Header />
       <Outlet />
       <Footer />
