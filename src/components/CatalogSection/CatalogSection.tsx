f5fff43df2df51902b8801d5e80041ba7e3ccc00
Nikita <nikita456nikita@yandex.ru>
2024-09-20 17:35:18 +0300

    feat: add loader

--- src/components/CatalogSection/CatalogSection.tsx
+++ src/components/CatalogSection/CatalogSection.tsx
@@ -4,22 +4,26 @@ import { CatalogSectionTitle } from 'components/CatalogSectionTitle/CatalogSecti
 import classes from './catalogSection.module.css';
 import { useEffect, useState } from 'react';
 import { Arts } from 'constants/interfaces';
+import { BallTriangle } from 'react-loader-spinner';
 
 export const CatalogSection = () => {
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [arts, setArts] = useState<Arts>({ data: [] });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleCurrentPage = (clickedPage: number) => {
     setCurrentPage(clickedPage);
   };
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(`https://api.artic.edu/api/v1/artworks?page=${currentPage}&limit=3`)
       .then((data) => {
         return data.json();
       })
       .then((data: Arts) => {
         setArts(data);
+        setIsLoading(false);
       });
   }, [currentPage]);
 
@@ -29,8 +33,21 @@ export const CatalogSection = () => {
         firstParagraph="Topics for you"
         secondParagraph="Our special gallery"
       />
-      <div>
-        <CatalogItemList arts={arts} />
+      <div className={classes.galleryWrapper}>
+        {isLoading ? (
+          <BallTriangle
+            height={100}
+            width={100}
+            radius={5}
+            color="rgba(241, 121, 0, 1)"
+            ariaLabel="ball-triangle-loading"
+            wrapperStyle={{}}
+            wrapperClass={classes.loader}
+            visible={true}
+          />
+        ) : (
+          <CatalogItemList arts={arts} />
+        )}
         <CatalogPagination
           currentPage={currentPage}
           handleCurrentPage={handleCurrentPage}
