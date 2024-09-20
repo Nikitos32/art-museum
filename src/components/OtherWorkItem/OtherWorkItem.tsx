c28cae34b1e17b9d62e1c7b73aba92efefbb09e2
Nikita <nikita456nikita@yandex.ru>
2024-09-20 17:45:23 +0300

    feat: add info for other work section

--- src/components/OtherWorkItem/OtherWorkItem.tsx
+++ src/components/OtherWorkItem/OtherWorkItem.tsx
@@ -1,15 +1,20 @@
 import { CatalogItemInfo } from 'components/CatalogItemInfo/CatalogItemInfo';
 import classes from './otherWorkItem.module.css';
+import { ArtItem } from 'constants/interfaces';
 
-export const OtherWorkItem = () => {
+interface OtherWorkItemProps {
+  art: ArtItem;
+}
+
+export const OtherWorkItem = ({ art }: OtherWorkItemProps) => {
   return (
     <div className={classes.otherWorkItemWrapper}>
       <img
-        src="src/assets/test-image.png"
+        src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
         alt="image"
         className={classes.otherWorkImage}
       />
-      <CatalogItemInfo />
+      <CatalogItemInfo art={art} />
     </div>
   );
 };
